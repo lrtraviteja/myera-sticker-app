28c3c2f7a11275d08ece9c498dd36722cf1f7317
lrtraviteja <lrtraviteja@gmail.com>
2025-06-07 14:31:19 +0530

    Implemented Adding, dragging, Deleting a Sticker on Canvas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+const imagePaths = [
+    '/stickers/christmas-decoration.png',
+    '/stickers/detective.png',
+    '/stickers/gun.png',
+    ];
+
+
 function App() {
 
   const [stickersOnCanvas, setSticker] = useState([]);
@@ -21,16 +28,27 @@ function App() {
     ]);
   };
 
-
   const downloadImage = () => {
     const uri = stageRef.current.toDataURL();
     const link = document.createElement('a');
-    link.download = 'canvas.png';
+    link.download = 'Canvas.png';
     link.href = uri;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
+
+  const updateStickerPosition = (id, newX, newY) => {
+    setSticker((prev) =>
+      prev.map((s) => (s.id === id ? { ...s, x: newX, y: newY } : s))
+    );
+  };
+
+  const deleteSticker = (id) => {
+    setSticker((prev) => prev.filter((s) => s.id !== id));
+  };
+
+
 
   return (
     <div className="App">
